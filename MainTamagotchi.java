ad9303842dd8e2d7a7e6e9fac0863ddb9d359f71
rubenavone <navone_ruben@msn.com>
2019-02-11 11:17:33 +0100

    first commit

---
+++ MainTamagotchi.java
@@ -0,0 +1,10 @@
+package tamagotchi;
+
+public class MainTamagotchi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Tamagotchi.java
@@ -0,0 +1,37 @@
+package tamagotchi;
+
+public class Tamagotchi {
+	
+		// Valeure comprise de -5 a 5 //
+		
+		private int faim;
+		private int poid;
+		private int propre;
+		private int bonheur;
+		public int getFaim() {
+			return faim;
+		}
+		public void setFaim(int faim) {
+			this.faim = faim;
+		}
+		public int getPoid() {
+			return poid;
+		}
+		public void setPoid(int poid) {
+			this.poid = poid;
+		}
+		public int getPropre() {
+			return propre;
+		}
+		public void setPropre(int propre) {
+			this.propre = propre;
+		}
+		public int getBonheur() {
+			return bonheur;
+		}
+		public void setBonheur(int bonheur) {
+			this.bonheur = bonheur;
+		}
+		
+		
+}
