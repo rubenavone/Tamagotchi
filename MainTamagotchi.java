4b7d53c34bae5a86a694c82c18331666b211f560
rubenavone <navone_ruben@msn.com>
2019-02-11 14:03:24 +0100

    Commit de survie

--- MainTamagotchi.java
+++ MainTamagotchi.java
@@ -3,8 +3,21 @@ package tamagotchi;
 public class MainTamagotchi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * Un tamagotchi:
+		 * naissance - mort 
+		 * features, pouvoir le nourrir, laver, sport et jeux.
+		 * 
+		 */
+		Tamagotchi tama = new Tamagotchi() ;
+		
+		System.out.println("Choisis le sex de ton tamagotchi !!");
+		tama.sex();
+		System.out.println("Merci de choisir le nom de votre tamagotchi !");
+		System.out.println(tama.eclosion());
+		System.out.println(tama);
+		
+		
 	}
 
 }
--- Tamagotchi.java
+++ Tamagotchi.java
@@ -1,37 +1,207 @@
 package tamagotchi;
 
+import java.util.Scanner;
+
 public class Tamagotchi {
+
+	static Scanner scan = new Scanner(System.in);
+	// Valeure comprise de -5 a 5 //
+
+	private int faim;
+	private int poid;
+	private int propre;
+	private int bonheur;
+	private int muscle;
+	private int fatigue;
+	private String sex;
+	private String nom;
+
+
+
+	/////GET & SET//////
+
+	public int getFaim() {
+		return faim;
+	}
+	public void setFaim(int faim) {
+		this.faim = faim;
+	}
+	public int getPoid() {
+		return poid;
+	}
+	public void setPoid(int poid) {
+		this.poid = poid;
+	}
+	public int getPropre() {
+		return propre;
+	}
+	public void setPropre(int propre) {
+		this.propre = propre;
+	}
+	public int getBonheur() {
+		return bonheur;
+	}
+	public void setBonheur(int bonheur) {
+		this.bonheur = bonheur;
+	}
+	public int getMuscle() {
+		return muscle;
+	}
+	public void setMuscle(int muscle) {
+		this.muscle = muscle;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getFatigue() {
+		return fatigue;
+	}
+	public void setFatigue(int fatigue) {
+		this.fatigue = fatigue;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	//////END//////
 	
-		// Valeure comprise de -5 a 5 //
-		
-		private int faim;
-		private int poid;
-		private int propre;
-		private int bonheur;
-		public int getFaim() {
-			return faim;
+	//ECLOSION//
+	public String eclosion() {
+
+		String user = "";		
+		nom = scan.nextLine();		
+		setNom(nom);
+		user = "Votre tamagotchi sort de ça coquille !!!" + "il s'appelle " + getNom();
+
+		faim = 0;
+		poid = 0;
+		propre = 0;
+		bonheur = 0;
+		muscle = 0;
+		return user;
+	}
+	//END//
+	
+	//CHOIX DU SEX//
+	public void sex() {
+		String choix;
+		System.out.println("voulez vous choisir le sexe de votre tamagotchi ?");
+		choix = scan.nextLine().toLowerCase();
+
+		if (choix.equals("oui")) {
+
+			System.out.println("Merci d'entrée votre choix (Male ou femelle ?");
+			choix = scan.nextLine().toLowerCase();
+			if(choix.equals("male")|| choix.equals("femelle"))
+
+				setSex(choix);
+			System.out.println("Votre tamagotchi est "+ getSex());
 		}
-		public void setFaim(int faim) {
-			this.faim = faim;
+
+		else  {
+			System.out.println(choixAuto());
 		}
-		public int getPoid() {
-			return poid;
+	}
+	public String choixAuto() {
+		int random = (int)(Math.random()*2+1);
+		String sex ="";
+		switch(random) {
+		case 1: setSex("male");
+		break;
+		case 2: setSex("femelle");
+		break;
+
+		}return sex;
+	}
+	//////END//////
+
+	///LES ACTIVITE///
+	public void manger() {
+		faim ++;
+		bonheur ++;
+		propre --;
+		poid ++;
+	}
+	public void dormir() {
+		bonheur ++;
+		faim --;
+		poid ++;
+		propre ++;
+
+	}
+	public void activiteSportive() {
+		fatigue --;
+		propre --;
+		poid --;
+		faim --;
+	}
+	public void douche() {
+		propre ++;
+		bonheur ++;
+		fatigue --;
+
+	}
+	public void jeux() {
+		bonheur += 2;
+		fatigue -= 2;
+		propre --;
+		faim ++;
+	}
+	//////END//////
+
+	///LES STATUS///
+	public void statusFaim() {
+		String status = "";
+		switch (faim) {
+		case -5:
+			status = nom + "Votre tamagotchi est mort";
+			break;
+		case -3: case -4: 
+			status = nom + "Votre tamagotchi agonise";
+			break;
+		case 0: case 1: 
+			status = nom + "Votre Tamagotchi est rassasié !"
+			break;
+		case 2: case 3:
+			
+			break;
 		}
-		public void setPoid(int poid) {
-			this.poid = poid;
-		}
-		public int getPropre() {
-			return propre;
-		}
-		public void setPropre(int propre) {
-			this.propre = propre;
-		}
-		public int getBonheur() {
-			return bonheur;
-		}
-		public void setBonheur(int bonheur) {
-			this.bonheur = bonheur;
-		}
-		
-		
+	}
+	public void statusPoid() {
+
+	}	
+	public void statusPropre() {
+
+	}
+	public void statusBonheur() {
+
+	}
+	public void statusMuscle() {
+
+	}
+	public void statusFatigue() {
+
+	}
+	//////END//////
+	
+	
+	
+	
+	/**
+	 * Essaie vue avec agnes
+	 */
+
+	@Override
+	public String toString() {
+		return "Tamagotchi [faim=" + faim + ", poid=" + poid + ", propre=" + propre + ", bonheur=" + bonheur
+				+ ", muscle=" + muscle + ", fatigue=" + fatigue + ", sex=" + sex + ", nom=" + nom + "]";
+	}
+
+
 }
